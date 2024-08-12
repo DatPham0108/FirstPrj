1492ddbf14e658a38d430a5e5e2923fca134ec5d
DatPham0108 <23631941.dat@student.iuh.edu.vn>
2024-08-12 23:29:49 +0700

    https://github.com/DatPham0108/FirstPrj.git

---
+++ src/wellcome.java
@@ -0,0 +1,6 @@
+public class wellcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Pham Tan Dat!") ;
+        System.out.println("Wellcome to the DHKTPM19A!") ;
+    }
+}
